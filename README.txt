853cec1a2725a64d5069b0be4937af76943ee2bd
mcatovic <momo2477@live.com>
2016-03-06 23:24:40 -0500

    Added files via upload

---
+++ README.txt
@@ -0,0 +1,108 @@
+Here is a simple Python script that is used with a Raspberry Pi to activate and light up a modified NHL Goal Light each time your favorite NHL team scores! You can run the script in the background or have it displayed in the foreground if you have a monitor connected. I have mine running in the background. The script also displays the score of previous, current and recently finished NHL games
+
+!
+
+So what are the delay times between the actual goal being scored and when the horn sounds? All games where watched live on tv, both East Coast & West Coast games, with a sample size of 36 goals:
+
+Min: 3 seconds
+Max: 1 minute 2 seconds
+Average: 28 seconds
+
+Full list of goal times at end of file.
+-----------------------------------------------------------------------------------------
+
+
+Requirements/Materials
+:
+
+-NHL Goal Light purchased from: http://us.thegoallight.com/store/#!/The-Goal-Light-XR/p/55401260/category=1620116
+	*I purchased mine from hockey giant and used a 20% off coupon. So you can find it cheaper!
+
+-Raspberry Pi with a WiFi Dongle that has the following installed:
+	Operating System: Raspbian
+	Python 2.7
+ +
+	Pip- Pip is already installed if you're using Python 2 >=2.7.9 or Python 3 >=3.4 downloaded from python.org, but you'll need to upgrade pip.
+	Requests
+- Open the terminal, Install with: pip install requests
+	Colorama
+- Open the terminal, Install with: pip install colorama
+
+-Power cable for the Raspberry Pi
+
+-At least 2 Dupont cables male to female 200mm+ length (Buy at least 10 so if you mess up, you have others to work with)
+
+-At least 1 Three Pin NPN Transistors 2N3904 (Buy at least 10 so if you mess up, you have others to work with)
+
+-At least 1 15k Ohm Resistors (Buy at least 10 so if you mess up, you have others to work with)
+
+-Soldering Iron & solder
+
+-Optional- Raspberry Pi case
+-----------------------------------------------------------------------------------------
+
+
+Instructions:
+
+Setting up the Raspberry Pi:
+
+Follow this link, https://www.raspberrypi.org/help/noobs-setup/ , to set up your Raspberry Pi if you don't know how. It's a straight forward tutorial and easy to follow. There are many tutorials out there on how to do this. Make sure you connect it to the internet also. Google "How to setup Raspberry Pi WiFi/Wireless". You WILL NEED to set up a Static IP address. If you don't set up a Static IP address, the IP address on the Raspberry Pi is set automatically and it will change. This will give you trouble when connecting remotely. So make sure you set up a Static IP address.
+
+There are two set of instructions inside the Instructions folder. If you purchased/have the older version of the NHL Goal Light follow the 'Remote Instructions'. The older versions of the Goal Light do NOT have a RESET button on the light. If you have the newest version of the Goal Light, your goal light WILL have a reset button. Follow the 'Reset Button Instructions'.
+
+Why does it matter? If you have the newest version of the Goal Light you will notice there is a Reset Button. In order to save electricity or batteries, if the light isn't activated within 3 hours, the goal light will go into 'Sleep Mode' which disables the remote and all of the lights functions until the reset button is pushed. Obviously NHL teams don't play 24/7 so it would be an inconvenience to have to push that reset button every day/3 hours so the program works. The 'Reset Button Instructions' work around that feature so you don't have to press it at all.
+
+
+-----------------------------------------------------------------------------------------
+
+
+Tested on
+ Raspberry Pi 2 with Raspbian installed via NOOBS_v1_5_0 and Python 2.7.9
+
+If you only want to see games for today you can run the script with the `--today-only` flag
+
+. Example: python /path/Scoreboard.py --today-only
+
+If you want to "set it and forget it" remotely you will have to download this nifty program called Screen onto the Raspberry Pi. Screen allows you to start a processes remotely, disconnect from the SSH, and have the instance continue running even after you disconnect your SSH session. Without this, once you exit the SSH session, all processes are stopped on the Raspberry Pi. Google "Using Screen with Raspberry Pi to avoid leaving SSH sessions open" for more details.
+
+
+-----------------------------------------------------------------------------------------
+
+I hope this tutorial will help you fully. If you have any questions feel free to email me at momo2477@live.com If you feel inclined to donate, you can donate by following this link paypal.me/mcatovic
+Thanks and enjoy!
+
+List of all goals in seconds. The absolute max delay can be 62 seconds. This program refreshes 2 seconds after NHL.com updates their scores. They update their scores every 60 seconds.
+
+3,7,9,10,13,14,15,19,22,25,25,25,26,30,31,32,32,33,34,36,40,45,46,47,50,50,51,54,60,62 
+
+
+*I changed the code around mid testing to create a more efficient refresh time, here is a small sample size of the restults after I changed the code around.
+
+6,12,27,4,5,11 
+
+
+
+
+-----------------------------------------------------------------------------------------
+
+The MIT License (MIT)
+Copyright (c) 2016 Muhamed Catovic
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ Scoreboard.py
@@ -0,0 +1,396 @@
+team = 'Sabres' # Select the team you want
+# Avalanche, Blackhawks, Blue Jackets, Blues, Bruins, Canadiens, Canucks, Capitals, Coyotes, Devils, Ducks, Flames, Flyers, Hurricanes, Islanders, Jets, Kings, Lightning, Maple Leafs ,Oilers, Panthers, Penguins, Predators, Red Wings, Sabres, Senators, Sharks, Stars, Wild
+
+
+from colorama import init, Fore, Style
+import datetime
+import json
+import os
+import platform
+import sys
+import time
+import requests
+import RPi.GPIO as GPIO
+import socket 
+
+i = datetime.datetime.now()  # date and time formatting http://www.cyberciti.biz/faq/howto-get-current-date-time-in-python/
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT) #Set the Raspberry Pi GPIO pin 7 as output
+
+refresh_time = 21600  # 6 hours Refresh time (seconds), NHL API refresh is every 60 seconds
+api_url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812'
+api_headers = {'Host': 'live.nhle.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36', 'Referer': 'http://www.nhl.com/ice/scores.htm'}
+
+#Home team variables
+old_score = 0
+
+team_playing = False
+
+#Away team variables
+away_old_score = 0
+
+
+show_today_only = False
+
+
+def main():
+	global refresh_time
+	global team
+	global team_playing
+	clear_screen()
+	
+	# Format dates to match NHL API style:
+	
+	# Todays date
+	t = datetime.datetime.now()
+	todays_date = "" + t.strftime("%A") + " " + "%s/%s" % (t.month, t.day)
+	
+	# Yesterdays date
+	y =y = t - datetime.timedelta(days=1)
+	yesterdays_date = "" + y.strftime("%A") + " " + "%s/%s" % (y.month, y.day)
+	
+	while True:
+				
+			try:
+				r = requests.get(api_url, headers=api_headers) #making sure there is a connection with the API
+			except (requests.ConnectionError): 
+				print ("Could not get response from NHL.com trying again...")
+				time.sleep(5)
+				continue
+			except(requests.HTTPError):
+				print ("HTTP Error when loading url. Please restart program. ")
+				sys.exit(0)
+			except(requests.Timeout):
+				print ("The request took too long to process and timed out. Trying again... ")
+				time.sleep(5)
+			except(socket.error):
+				print ("Could not get response from NHL.com trying again...")
+				time.sleep(5)
+			except(requests.RequestException):
+				print ("Unknown error. Please restart the program. ")
+				sys.exit(0)
+			
+			
+			# We get back JSON data with some JS around it, gotta remove the JS
+			json_data = r.text
+			
+			# Remove the leading JS
+			json_data = json_data.replace('loadScoreboard(', '')
+			
+			# Remove the trailing ')'
+			json_data  = json_data[:-1]
+			
+			data = json.loads(json_data)
+			for key in data:
+				if key == 'games':
+					for game_info in data[key]:
+					
+						# Assign more meaningful names
+						game_clock = game_info['ts']
+						game_stage = game_info['tsc']
+						status = game_info['bs']
+						
+						away_team_locale = game_info['atn']
+						away_team_name = game_info['atv'].title()
+						away_team_score = game_info['ats']
+						away_team_result = game_info['atc']
+						
+						
+						home_team_locale = game_info['htn']
+						home_team_name = game_info['htv'].title()
+						home_team_score = game_info['hts']
+						home_team_result = game_info['htc']
+						
+						# Fix strange names / locales returned by NHL
+						away_team_locale = fix_locale(away_team_locale)
+						home_team_locale = fix_locale(home_team_locale)
+						away_team_name = fix_name(away_team_name)
+						home_team_name = fix_name(home_team_name)
+						
+						
+						
+						
+						
+						# Show games from Yesterday and today or just today
+						if (yesterdays_date in game_clock.title() and not show_today_only) or todays_date in game_clock.title() or 'TODAY' in game_clock or 'LIVE' in status:
+						
+							header_text = away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
+							
+							# Different displays for different states of game:
+							# Game from yesterday, ex: on YESTERDAY, MONDAY 4/20 (FINAL 2nd OT)
+							# Game from today finished, ex: TODAY (FINAL 2nd OT)
+							if 'FINAL' in status:
+								if yesterdays_date in game_clock.title():
+									header_text += '\nYESTERDAY, ' + game_clock + ' '
+								elif todays_date in game_clock.title() or 'TODAY' in game_clock:
+									header_text += '\nTODAY '
+								header_text += '(' + status + ')'
+							
+							
+							
+							
+							# Upcoming game, ex: TUESDAY 4/21, 7:00 PM EST)
+							elif 'DAY' in game_clock:
+								header_text += Fore.YELLOW + '\n(' + game_clock + ', ' + status + ' EST)' + Fore.RESET
+							
+							# Last 5 minutes of game and overtime, ex: (1:59 3rd PERIOD) *in red font*
+							elif 'critical' in game_stage:
+								header_text += '\n(' + Fore.RED + game_clock + ' PERIOD' + Fore.RESET + ')'
+							
+							
+							# Any other time in game, ex: (10:34 1st PERIOD)
+							else:
+								header_text += Fore.YELLOW + '\n(' + game_clock + ' PERIOD)' + Style.RESET_ALL
+							
+							
+							print(header_text)
+							
+							
+							
+							
+							
+							# Highlight the winner of finished games in green, and games underway in blue:
+							# Away team wins
+							if away_team_result == 'winner':
+								print(Style.BRIGHT + Fore.GREEN + away_team_name + ': ' + away_team_score + Style.RESET_ALL)
+								print(home_team_name + ': ' + home_team_score)
+							
+							
+							# Home team wins
+							elif home_team_result == 'winner':
+								print(away_team_name + ': ' + away_team_score)
+								print(Style.BRIGHT + Fore.GREEN + home_team_name + ': ' + home_team_score + Style.RESET_ALL)
+							
+							
+							# Game still underway
+							elif 'progress' in game_stage or 'critical' in game_stage:
+								print(Fore.CYAN + away_team_name + ': ' + away_team_score)
+								print(home_team_name + ': ' + home_team_score + Fore.RESET)
+								game_home(home_team_name,home_team_score,game_clock,status)
+								game_away(away_team_name,away_team_score,game_clock,status)
+							
+															
+							# Game hasn't yet started
+							else:
+								print(away_team_name + ': ' + away_team_score)
+								print(home_team_name + ': ' + home_team_score)
+								game_home(home_team_name,home_team_score,game_clock,status)
+								game_away(away_team_name,away_team_score,game_clock,status)
+							print('')
+							if ('FINAL' in status) and (away_team_name == team or home_team_name == team) and (yesterdays_date in game_clock.title() or todays_date in game_clock.title() ):
+								print "Game over!"
+								refresh_time = 21600
+								team_playing = False
+								print "Refresh in: " + str(refresh_time) + " seconds (6 hours)"
+								print "Team playing: " + str(team_playing)
+								print ""
+
+							
+							
+							
+							
+							
+			
+			if team_playing == False:
+				print ("Current time: " + str(datetime.datetime.now()))
+				refresh_time = 21600 # 6 hours
+				print team + " are not playing. Refreshing in " + str(refresh_time) + " seconds (6 hours)."
+			# Perfrom the refresh
+			time.sleep(refresh_time)
+
+			
+			
+def clear_screen():
+	if platform.system() == 'Windows':
+		os.system('cls')
+	else:
+		os.system('clear')
+
+
+def print_help():
+	print('By default games from yesterday and today will be displayed.')
+	print('')
+	print('If you want to see games from just today run the program with ')
+	print('the "--today-only" flag.')
+
+
+def fix_locale(team_locale):
+	# NHL API forces team name in locale for both New York teams, i.e. locale + name == "NY Islanders islanders"
+	if 'NY ' in team_locale:
+		return 'New York'
+	
+	if 'Montr' in team_locale:
+		return 'Montreal'
+	
+	return team_locale
+
+
+
+
+def game_away(away_team_name, away_team_score, game_clock, status):
+	global refresh_time
+	global away_old_score
+	global team_playing
+	current_time = time.strftime("%I:%M:%S")
+	game_time = str(status)
+	game_date = game_clock
+	now = datetime.datetime.now()
+	now_second = now.second
+	
+	if team in away_team_name:
+		print""
+		print""
+		print ("Current time: " + current_time) 
+		team_playing = True
+		
+		if (game_date != "TODAY" and game_time != "LIVE"):
+			print("Game time is quite aways away!")
+			refresh_time = 21600 # 6 hours
+			away_old_score = 0
+			print "Refreh time: " + str(refresh_time) + " seconds (6 hours)"
+			print "Away old_score: " + str(away_old_score)
+		
+		
+		elif (game_date == "TODAY"): #Game time is near
+			refresh_time = 3600 # 1 hours
+			print "It is game day!"
+			print "Refresh time is : " + str(refresh_time) + " seconds (1 hour)"
+		
+		
+		elif (game_date == "PRE GAME"): #30 minutes to game time
+			refresh_time = 300 # 5 minutes
+			print "It is almost game time!"
+			print "Refresh time: " + str(refresh_time) + " seconds (5 minutes)"
+		
+		
+		else: # Game is underway
+			#The API url refreshes at exactly the next minute. So we calculate the time until the next minute, set it to refresh then
+			refresh_time = 63 - now.second - now.microsecond/1e6
+			print "Refresh time: " + str(refresh_time) + " seconds"
+			print "Away old_score: " + str(away_old_score)
+			
+			
+			if int(away_old_score) < int(away_team_score): # If the old score < the new score, a goal was scored
+				print team + " have scored a goal!"
+				GPIO.output(7, True) # Activate the light
+				time.sleep(1)
+				GPIO.output(7, False) # Turn the rPI pin off so we can use it again
+				away_old_score = int(away_team_score) # Set the old_score to be the current score
+				print "Away old_score: " + str(away_old_score)
+				
+	elif team_playing == False: # The selected team isn't playing
+		refresh_time = 21600 # 6 hours
+		away_old_score = 0
+		print "Away old_score: " + str(away_old_score)
+		print "Not " + team + " gameday!! Refresh in: " + str(refresh_time) + " seconds (6 hours)"
+
+
+
+
+
+
+def game_home(home_team_name, home_team_score, game_clock, status):
+	global refresh_time
+	global old_score
+	global team_playing
+	current_time = time.strftime("%I:%M:%S")
+	game_time = str(status)
+	game_date = game_clock
+	now = datetime.datetime.now()
+	now_second = now.second	
+	
+	if team in home_team_name:
+		print""
+		print""
+		print ("Current time: " + current_time) 
+		team_playing = True
+		
+		if (game_date != "TODAY" and game_time != "LIVE"):
+			print("Game time is quite aways away!")
+			refresh_time = 21600 # 6 hours
+			old_score = 0
+			print "Refreh time: " + str(refresh_time) + " seconds (6 hours)"
+			print "Home old_score: " + str(old_score)
+		
+		
+		elif (game_date == "TODAY"): #Game time is near
+			refresh_time = 3600 # 1 hours
+			print "It is game day!"
+			print "Refresh time is : " + str(refresh_time) + " seconds (1 hour)"
+		
+		
+		elif (game_date == "PRE GAME"): #30 minutes to game time
+			refresh_time = 300 # 5 minutes
+			print "It is almost game time!"
+			print "Refresh time: " + str(refresh_time) + " seconds (5 minutes)"
+		
+		
+		else: # Game is underway
+			print "Now: " + str(now)
+			print "Now.second: " + str(now_second)
+			
+			#The API url refreshes at exactly the next minute. So we calculate the time until the next minute, set it to refresh then
+			refresh_time = 63 - now.second - now.microsecond/1e6
+			print "Time untill next minute: " + str(refresh_time)
+			print "Refresh time: " + str(refresh_time) + " seconds"
+			print "Home old_score: " + str(old_score)
+			
+			
+			if int(old_score) < int(home_team_score): # If the old score < the new score, a goal was scored
+				print team + " have scored a goal!"
+				GPIO.output(7, True) # Activate the light
+				time.sleep(1)
+				GPIO.output(7, False) # Turn the rPI pin off so we can use it again
+				old_score = int(home_team_score) # Set the old_score to be the current score
+				print "Home old_score: " + str(old_score)
+				
+	elif team_playing == False: # The selected team isn't playing
+		refresh_time = 21600 # 6 hours
+		old_score = 0
+		print "Home old_score: " + str(old_score)
+		print "Not " + team + " gameday!! Refresh in: " + str(refresh_time) + " seconds (6 hours)"
+
+
+
+
+
+
+def fix_name(team_name):
+	#Change 'redwings' to 'Red Wings'
+	if 'wings' in team_name:
+		return 'Red Wings'
+	
+	if 'jackets' in team_name:
+		return 'Blue Jackets'
+	
+	if 'leafs' in team_name:
+		return 'Maple Leafs'
+	
+	return team_name
+
+
+
+def parse_arguments(arguments):
+	global show_today_only
+	for x in range(1, len(arguments)):
+		argument = arguments[x]
+		
+		if argument == '--help' or argument == '-h':
+			print_help()
+			sys.exit(0)
+		elif argument == '--today-only':
+			show_today_only = True
+
+
+
+
+if __name__ == '__main__':
+    # Initialize Colorama
+    init()
+
+    # Parse any arguments provided
+    parse_arguments(sys.argv)
+
+    # Start the main loop
+    main()
